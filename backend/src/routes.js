8129b33be631e74dbc181ed10f4e92fbba4dac57
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-24 18:08:21 -0300

    finalizado o backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,10 +3,17 @@ const routes = express.Router()
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/IncidentController')
+const SessionControlles = require('./controllers/SessionController')
 
+//Login
+routes.post('/session',SessionControlles.create)
+// Cadastrar e listar ongs
 routes.get('/ongs',OngController.index)
 routes.post('/ongs',OngController.create)
-
+// listar casos de uma ong especifica
+routes.get('/profile',ProfileController.index)
+// Criar listar e deletar casos
 routes.get('/incidents',IncidentController.index)
 routes.post('/incidents', IncidentController.create)
 routes.delete('/incidents/:id',IncidentController.delete)
