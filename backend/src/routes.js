5b068a6fe354ca329aa4e1c1d6791dbaa7f5d840
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-24 15:48:53 -0300

    backend commit 2

---
+++ backend/src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const routes = express.Router()
+
+routes.post('/users',(req,res)=>{
+    const body = req.body
+
+    console.log(body)
+
+    return res.json(
+        {
+            evento:'Semana Omnistack 11.0',
+            aluno:'Jheni Jhenosa'
+        }
+    )
+})
+
+module.exports = routes
