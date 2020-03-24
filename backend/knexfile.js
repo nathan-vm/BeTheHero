e5cc71ac87603851f8ac348277e7c2f5f275c133
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-24 16:18:19 -0300

    criado o database sqlite

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,8 +5,13 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './src/database/db.sqlite3'
+    },
+    migrations:{
+      directory:'./src/database/migrations',
+      
+    },
+    useNullAsDefault: true
   },
 
   staging: {
