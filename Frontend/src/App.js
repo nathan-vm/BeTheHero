ad79a9c6870e7bf9eb7fc18eb6afc01cd286b7c8
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-25 12:37:16 -0300

    logon

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Header from './Header'
-import { useState } from 'react';
+import Logon from './pages/Logon'
+
 
 function App() {
-  let [counter,setCounter] = useState(0)
-// Array [valor,funcaodeAtualizacao]
-  function increment(){
-    setCounter(counter+1)
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+
+      <Logon></Logon>
+      
+
   );
 }
 
