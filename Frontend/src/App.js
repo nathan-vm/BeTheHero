beb6e2a1c1e4a281da8351c96b5405152e6cb78a
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-25 13:01:16 -0300

    sei la

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logon from './pages/Logon'
-
+import './global.css'
 
 function App() {
   return (
--- Frontend/src/pages/Logon/index.jsx
+++ Frontend/src/pages/Logon/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import './styles.css'
+
+//import herosImg from '../../assets/heros.png'
+
+
 export default function Logon(){
     return(
-        <h1>Logon</h1>
+        <div className="logon-container">
+            <section className="form">
+
+            </section>
+            <img src='../../assets/heros.png' alt="Heros"/>
+        </div>
     )
 }
