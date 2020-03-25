c4f6c866d43c3f7d06ca5863a563ba8de43ec8b4
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-25 14:15:04 -0300

    logon feito

--- Frontend/src/pages/Logon/index.jsx
+++ Frontend/src/pages/Logon/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import './styles.css'
 
-//import herosImg from '../../assets/heros.png'
+import {FiLogIn} from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg'
+import herosImg from '../../assets/heroes.png'
 
 
 export default function Logon(){
     return(
         <div className="logon-container">
             <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
 
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder='Sua ID'/>
+                    <button className="button" type="submit">Entrar</button>
+                    
+                    
+                    <a href="/register"> <FiLogIn size={16} color="#e02041"/> Não tenho cadastro</a>
+                </form>
             </section>
-            <img src='../../assets/heros.png' alt="Heros"/>
+            <img src={herosImg} alt="Heros"/>
         </div>
     )
 }
