210c625d0c7222a3e30f0a87c5337740ec4e2707
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-26 11:04:43 -0300

    .

--- Backend/src/controllers/ProfileController.js
+++ Backend/src/controllers/ProfileController.js
@@ -6,8 +6,6 @@ module.exports = {
 
         const incidents = await connection('incidents').where('ong_id',ong_id).select('*')
 
-        console.log("ong_id, request.headers")
-
         return response.json(incidents)
     }
 }
