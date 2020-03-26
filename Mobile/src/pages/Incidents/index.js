d5b2dd218eac20caafd603c76c6bcc6c5616478e
nathan-vm <nathan.v.marcelino@gmail.com>
2020-03-26 14:55:02 -0300

    estilo

--- Mobile/src/pages/Incidents/index.js
+++ Mobile/src/pages/Incidents/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import {View,Image, Text} from 'react-native'
+
+import {View,FlatList,Image, Text, TouchableOpacity} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import { Feather } from '@expo/vector-icons'
 import logoImg from '../../assets/logo.png'
 import styles from './styles'
 
@@ -20,6 +24,34 @@ export default function Incidents(){
             <Text style={styles.description}>
                 Escolha um dos casos abaixo e salve o dia.
             </Text>
+
+            <FlatList
+                style={styles.incidentList}
+                data={[1,2,3]}
+                keyExtractor={incident=>incident}
+                showsVerticalScrollIndicator={false}
+                renderItem={()=>(
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity 
+                        style={styles.detailsButton}
+                        onPress={()=>{}}>
+                            <Text style={styles.detailsButtonText}>
+                            Ver Mais detalhes
+                            </Text>
+                            <Feather name='arrow-right' size={16} color="#e02041"/>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     )
 }
